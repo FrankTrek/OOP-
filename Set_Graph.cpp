c2175cf4f9fb2c62a6a7c82021e8ebb527308f06
FrankTrek <49267592+FrankTrek@users.noreply.github.com>
2019-04-14 11:44:53 +0800

    Add files via upload

--- Set_Graph.cpp
+++ Set_Graph.cpp
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 #include "Set_Graph.h"
+#include "Const.h"
+#include "Node.h"
 namespace Computational_Graph{
     
     void Set_Graph::Initalize_Ph(const string& name)               //对于Ph(n名字为name)进行初始化；
@@ -21,7 +23,7 @@ namespace Computational_Graph{
         {
             graph[name].Mode = Placehold;
             graph[name].Nodename = name;
-            graph[name].node = std::make_shared<Placeholder>(new Placeholder(name));
+            graph[name].node = std::make_shared<Placeholder>(name);
         }
     }
     void Set_Graph::Initalize_V(const string& name, float num)               //对于V(n名字为name)进行初始化；
@@ -35,7 +37,7 @@ namespace Computational_Graph{
         {
             graph[name].Nodename = name;
             graph[name].Mode = Varible;
-            graph[name].node = std::make_shared<Variable>(new Variable(num,name));
+            graph[name].node = std::make_shared<Variable>(num,name);
         }
     }
     void Set_Graph::Initalize_C(const string& name, float num)               //对于C(n名字为name)进行初始化；
@@ -49,12 +51,12 @@ namespace Computational_Graph{
         {
             graph[name].Mode = Const;
             graph[name].Nodename = name;
-            graph[name].node = std::make_shared<Constant>(new Constant(num,name));
+            graph[name].node = std::make_shared<Constant>(num,name);
         }
     }
     
     
-    void Set_Graph::ADD(const string& obj, const string& p1, const string& p2)
+    void Set_Graph::ADD(const string& obj, const string& p1, const string& p2)       //加法操作
     {
         auto x = graph.find(obj);  auto pt1 = graph.find(p1); auto pt2 = graph.find(p2);
         if(x!=graph.end())           //被重定义错误
@@ -71,9 +73,127 @@ namespace Computational_Graph{
             graph[obj].Nodename = obj;
             BaseNode<float>* para1 = graph[p1].node.get();
             BaseNode<float>* para2 = graph[p1].node.get();
-            graph[obj].node = std::make_shared<Addition>(new Addition(obj,para1, para2));
+            graph[obj].node = std::make_shared<Addition>(obj,para1, para2);
         }
         
+    }
+    void Set_Graph::Multi(const string& obj, const string& p1, const string& p2)   //乘法z操作
+    {
+        auto x = graph.find(obj);  auto pt1 = graph.find(p1); auto pt2 = graph.find(p2);
+        if(x!=graph.end())           //被重定义错误
+        {
+            std::cerr<<"False Redefination\n";
+        }
+        else if (pt2==graph.end()||pt1 == graph.end())
+        {
+            std::cerr<<"False Parameter defination\n";
+        }
+        else
+        {
+            graph[obj].Mode = Operator;
+            graph[obj].Nodename = obj;
+            BaseNode<float>* para1 = graph[p1].node.get();
+            BaseNode<float>* para2 = graph[p1].node.get();
+            graph[obj].node = std::make_shared<Multiply >(obj,para1, para2);
+        }
+    }
+    
+    void Set_Graph::MINUS(const string& obj, const string& p1, const string& p2)     //减法z操作
+    {
+        auto x = graph.find(obj);  auto pt1 = graph.find(p1); auto pt2 = graph.find(p2);
+        if(x!=graph.end())           //被重定义错误
+        {
+            std::cerr<<"False Redefination\n";
+        }
+        else if (pt2==graph.end()||pt1 == graph.end())
+        {
+            std::cerr<<"False Parameter defination\n";
+        }
+        else
+        {
+            graph[obj].Mode = Operator;
+            graph[obj].Nodename = obj;
+            BaseNode<float>* para1 = graph[p1].node.get();
+            BaseNode<float>* para2 = graph[p1].node.get();
+            graph[obj].node = std::make_shared<Minus >(obj,para1, para2);
+        }
+    }
+    void Set_Graph::Div(const string& obj, const string& p1, const string& p2)     //除法z操作
+    {
+        auto x = graph.find(obj);  auto pt1 = graph.find(p1); auto pt2 = graph.find(p2);
+        if(x!=graph.end())           //被重定义错误
+        {
+            std::cerr<<"False Redefination\n";
+        }
+        else if (pt2==graph.end()||pt1 == graph.end())
+        {
+            std::cerr<<"False Parameter defination\n";
+        }
+        else
+        {
+            graph[obj].Mode = Operator;
+            graph[obj].Nodename = obj;
+            BaseNode<float>* para1 = graph[p1].node.get();
+            BaseNode<float>* para2 = graph[p1].node.get();
+            graph[obj].node = std::make_shared<Division >(obj,para1, para2);
+        }
+    }
+    
+    
+    void Set_Graph::Initalize_Op()          //第二阶段的操作
+    {
+        if(info[2]=="+")                 //为加法;
+        {
+            ADD(info[0], info[1], info[3]);
+        }
+        else if(info[2]=="*")                 //为乘法
+        {
+            Multi(info[0], info[1], info[3]);
+        }
+        else if(info[2]=="-")            //为减法
+        {
+             MINUS(info[0], info[1], info[3]);
+        }
+        else if(info[2]=="/")
+        {
+            Div(info[0], info[1], info[3]);
+        }
+        
+    }
+    
+    void Set_Graph::processing_Stage3()
+    {
+        if(info[0]=="EVAL")
+        {
+            int times = atoi(info[2].data());     //读取操作次数
+            for(int i = 1; i<= times; i++)
+            {
+                int pos = 2+(i-1)*2 + 1;       //此时读取的位置
+                string name = info[pos];
+                float num = atof(info[pos+1].data());
+                if(graph[name].Mode!=Placehold) std::cerr<<"Can not Initalize a non-PlaceHolder Object\n";
+                else graph[name].node->Initalize(num);
+            }
+            Compute(info[1]);
+        }
+        else if(info[0]=="SETCONSTANT")
+        {
+            string name = info[1];   //获得名字
+            float num = atof(info[2].data());
+            if(graph[name].Mode!=Varible) std::cerr<<"Can not setconstant a non-Varible Object\n";
+            else
+            {
+                graph[name].node->Initalize(num);
+            }
+            Answer.push_back(Minus_Max);
+        }
+        else if(info[0]=="SETANSWER")
+        {
+            string name = info[1];   //获得名字
+            int n = atoi(info[2].data()); //获得第n次的值
+            SetAnswer(name, n);
+            Answer.push_back(Minus_Max);
+        }
     }
     
 }
--- Set_Graph.h
+++ Set_Graph.h
@@ -17,6 +17,7 @@ namespace Computational_Graph{
         typedef Graph_Node<float> GNode;
         std::map<string, GNode> graph;          //图
         std::vector<string> info;              //处理的信息
+        std::vector<float> Answer;             //输出的信息
     public:
         Set_Graph() {}
         Set_Graph(std::vector<string> && a)       //直接传入信息的构造函数
@@ -27,20 +28,35 @@ namespace Computational_Graph{
         {
             info = std::move(a);
         }
-        void processing();                        //对输入字符串的处理  未完成
+        void processing_Stage3();                        //对输入字符串的处理  未完成
         void ADD(const string& obj, const string& p1, const string& p2);                //加法操作
+        void Multi(const string& obj, const string& p1, const string& p2);               //乘法操作
+        void MINUS(const string& obj, const string& p1, const string& p2);               //减法操作
+        void Div(const string& obj, const string& p1, const string& p2);                 //除法操作
         void Print(const string& name);           //执行打印操作  未完成
         //假装这里有一吨操作符
         void Compute(const string& a)             //对于表达式进行计算
         {
              float b = graph[a].node->Forward();
+             Answer.push_back(b);
              cout<<b<<std::endl;
         }
-        void Set_V(float a, const string& op);                       //对vVariable进行操作 op for 输入的具体操作 未完成
-        void Initalize();                          //未完成
+        void SetAnswer(const string& name, int n)                       //对Variable进行seranswer操作 未完成
+        {
+            if(Answer[n]<Minus_Max+0.0000001) std::cerr<<"Invalid Operation\n";
+            else if(graph[name].Mode== Varible) graph[name].node->Initalize(Answer[n]);
+            else std::cerr<<"Set_answer option only for Varible\n";            //如果不为Varible则报错
+        }
+        void Initalize_PVC();                          //未完成
+        void Initalize_Op();                           //未完成
         void Initalize_Ph(const string& name);               //对于Ph(n名字为name)进行初始化；
         void Initalize_V(const string& name, float num);               //对于V(n名字为name)进行初始化；
         void Initalize_C(const string& name, float num);               //对于C(n名字为name)进行初始化；
+        void Initalize_Input(const string& name, float num)           //在第三阶段对于输入进行初始化;
+        {
+           if(graph[name].Mode==Placehold) graph[name].node->Initalize(num);
+           else std::cerr<<"Invalid Operation\n";
+        }
         bool IsValid(const string& name)           //关于这个节点建立是否合法
         {
             return graph[name].node->IsCycle(graph[name].Nodename);
