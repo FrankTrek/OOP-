f7a6c70db0fc454ffce9ad7c28110f9d3f95302e
Lionjump0723 <49281380+Lionjump0723@users.noreply.github.com>
2019-04-17 20:00:24 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include "Const.h" 
-
+#include "Const.h"
+#include "Operation.h"
+#include "Operation2.h"
 using namespace Computational_Graph ;
 
 
@@ -8,7 +9,7 @@ using namespace Computational_Graph ;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-    Variable a(1.0f,"abcd") ; 
+    Variable a(1.0f,"abcd") ;
     Placeholder b("abcd") ;
     Constant c(1.0f,"ad") ;
     Constant d(1.2f,"cd") ;
@@ -24,5 +25,5 @@ int main() {
     b.debug_print() ;
     c.debug_print() ;
     d.debug_print() ;
-	return 0;
+    return 0;
 }
