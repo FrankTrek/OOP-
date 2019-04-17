b29f8b75ecf17a3b33373e26e12a350f98a5d7cb
Lionjump0723 <49281380+Lionjump0723@users.noreply.github.com>
2019-04-17 19:55:37 +0800

    Update BaseNode.h

--- BaseNode.h
+++ BaseNode.h
@@ -16,6 +16,8 @@
  *4.8 Z.设置了setflag接口
  *4.12 增加了检查是否出现有向回路的方法
  *4.12 将Struct中的指针改为shared_ptr
+ *4.13 补充极小量eps
+ *4.17 将Print()函数改为专门输出节点名的函数
  */
 
 
@@ -36,7 +38,8 @@ namespace Computational_Graph
     using std::istream;
     using std::cout;
     using std::endl;
-    const float Minus_Max = -100000000000;    //用于给定float下界以判断是否完成输入
+    const float Minus_Max = -100000000000;//用于给定float下界以判断是否完成输入
+    const float eps=1e-7;
     enum { Varible, Const, Operator ,Placehold};
     //写在之前的警告⚠️：必须在之后的继承类中补充关于前向传播的定义，否则继承类将依然成为虚类⚠️⚠️⚠️
     template <typename T>
@@ -95,9 +98,9 @@ namespace Computational_Graph
         {
             cout<<"Func Backward()  has not yet been defined\n";
         }
-        virtual void Print()          //打印信息的接口
+        virtual void Print()          //打印信息的接口 4.17 改为专门打印节点名
         {
-            cout<<"Func Print() has not yet been defined\n";
+            cout<<name;
         }
         //新增4.12 检查是否出现有向回路  true for 有
         bool IsCycle(const string& a)
@@ -130,7 +133,7 @@ namespace Computational_Graph
      *提供了前向传播的主要接口
      *为后向传播预留了借口
      *关闭了各种复制以及类型转换
-     *输入节点的初始话接口
+     *输入节点的初始化接口
      
      
      
