60fa833d43ab524ad0a4a234feeb4108aa5fe6d8
SAZDDDDDD <48305798+Xiaoyu-SZ@users.noreply.github.com>
2019-04-21 15:38:08 +0800

    Add files via upload

--- Set_Graph.cpp
+++ Set_Graph.cpp
@@ -6,6 +6,10 @@
 //  Copyright © 2019 王一诺. All rights reserved.
 //
 
+/*4.21 Cai. 补全了Set_Graph:: Operation类所有构造节点函数
+ *4.21 Cai. 用模版重写了Set_Graph:: 里所有的构造节点函数
+ 
+ */
 #include <stdio.h>
 #include "Set_Graph.h"
 #include "Const.h"
@@ -54,8 +58,8 @@ namespace Computational_Graph{
             graph[name].node = std::make_shared<Constant>(num,name);
         }
     }
-    
-    
+    //修改思路，把下面所有的重复代码全部
+    /*
     void Set_Graph::ADD(const string& obj, const string& p1, const string& p2)       //加法操作
     {
         auto x = graph.find(obj);  auto pt1 = graph.find(p1); auto pt2 = graph.find(p2);
@@ -72,7 +76,7 @@ namespace Computational_Graph{
             graph[obj].Mode = Operator;
             graph[obj].Nodename = obj;
             BaseNode<float>* para1 = graph[p1].node.get();
-            BaseNode<float>* para2 = graph[p1].node.get();
+            BaseNode<float>* para2 = graph[p2].node.get();
             graph[obj].node = std::make_shared<Addition>(obj,para1, para2);
         }
         
@@ -93,7 +97,7 @@ namespace Computational_Graph{
             graph[obj].Mode = Operator;
             graph[obj].Nodename = obj;
             BaseNode<float>* para1 = graph[p1].node.get();
-            BaseNode<float>* para2 = graph[p1].node.get();
+            BaseNode<float>* para2 = graph[p2].node.get();
             graph[obj].node = std::make_shared<Multiply >(obj,para1, para2);
         }
     }
@@ -114,7 +118,7 @@ namespace Computational_Graph{
             graph[obj].Mode = Operator;
             graph[obj].Nodename = obj;
             BaseNode<float>* para1 = graph[p1].node.get();
-            BaseNode<float>* para2 = graph[p1].node.get();
+            BaseNode<float>* para2 = graph[p2].node.get();
             graph[obj].node = std::make_shared<Minus >(obj,para1, para2);
         }
     }
@@ -134,31 +138,131 @@ namespace Computational_Graph{
             graph[obj].Mode = Operator;
             graph[obj].Nodename = obj;
             BaseNode<float>* para1 = graph[p1].node.get();
-            BaseNode<float>* para2 = graph[p1].node.get();
-            graph[obj].node = std::make_shared<Division >(obj,para1, para2);
+            BaseNode<float>* para2 = graph[p2].node.get();
+            graph[obj].node = std::make_shared<Division>(obj,para1, para2);
         }
     }
     
-    
-    void Set_Graph::Initalize_Op()          //第二阶段的操作
+    //add by Cai on 4.21
+    void Set_Graph::Cond(const std::string &obj, const std::string &p1, const std::string &p2, const std::string &p3) //cond操作
     {
+        auto x = graph.find(obj);
+        auto pt1 = graph.find(p1);
+        auto pt2 = graph.find(p2);
+        auto pt3 = graph.find(p3);
+        if(x!=graph.end())           //被重定义错误
+        {
+            std::cerr<<"False Redefination\n";
+        }
+        else if (pt3==graph.end()||pt2==graph.end()||pt1 == graph.end())
+        {
+            std::cerr<<"False Parameter defination\n";
+        }
+        else {
+            graph[obj].Mode = Operator;
+            graph[obj].Nodename = obj;
+            BaseNode<float>* para1 = graph[p1].node.get();
+            BaseNode<float>* para2 = graph[p2].node.get();
+            BaseNode<float>* para3 = graph[p3].node.get();
+            graph[obj].node = std::make_shared<COND>(obj,para1, para2, para3);
+        }
+    }
+    void Set_Graph::XIAOYU(const string& obj, const string& p1, const string& p2)
+    {
+        auto x = graph.find(obj);  auto pt1 = graph.find(p1); auto pt2 = graph.find(p2);
+        if(x!=graph.end())           //被重定义错误
+        {
+            std::cerr<<"False Redefination\n";
+        }
+        else if (pt2==graph.end()||pt1 == graph.end())
+        {
+            std::cerr<<"False Parameter defination\n";
+        }
+        else
+        {
+            graph[obj].Mode = Operator;
+            graph[obj].Nodename = obj;
+            BaseNode<float>* para1 = graph[p1].node.get();
+            BaseNode<float>* para2 = graph[p2].node.get();
+            graph[obj].node = std::make_shared<Xiaoyu>(obj,para1, para2);
+        }
+    }
+    */
+    //-----------------------------------------------------------------------------------------
+    void Set_Graph::Initalize_Op()          //第二阶段的操作
+    {//add by Cai on 4.21
         if(info[2]=="+")                 //为加法;
         {
-            ADD(info[0], info[1], info[3]);
+            //ADD(info[0], info[1], info[3]);
+            Jianli_2<Addition>(info[0], info[1], info[3]);
         }
         else if(info[2]=="*")                 //为乘法
         {
-            Multi(info[0], info[1], info[3]);
+            //Multi(info[0], info[1], info[3]);
+            Jianli_2<Multiply>(info[0], info[1], info[3]);
         }
         else if(info[2]=="-")            //为减法
         {
-             MINUS(info[0], info[1], info[3]);
+             //MINUS(info[0], info[1], info[3]);
+            Jianli_2<Minus>(info[0], info[1], info[3]);
         }
         else if(info[2]=="/")
         {
-            Div(info[0], info[1], info[3]);
+            //Div(info[0], info[1], info[3]);
+            Jianli_2<Division>(info[0], info[1], info[3]);
         }
-        
+        else if(info[2]=="<")
+        {
+            Jianli_2<Xiaoyu>(info[0], info[1], info[3]);
+        }
+        else if(info[2]=="<=")
+        {
+            Jianli_2<Xiaodeng>(info[0], info[1], info[3]);
+        }
+        else if(info[2]==">")
+        {
+            Jianli_2<Dayu>(info[0], info[1], info[3]);
+        }
+        else if(info[2]==">=")
+        {
+            Jianli_2<Dadeng>(info[0], info[1], info[3]);
+        }
+        else if(info[2]=="==")
+        {
+            Jianli_2<Dengyu>(info[0], info[1], info[3]);
+        }
+        else if(info[2]=="==")
+        {
+            Jianli_2<Budeng>(info[0], info[1], info[3]);
+        }
+        else if(info[1]=="SIN")
+        {
+            Jianli_1<SIN>(info[0], info[2]);
+        }
+        else if(info[1]=="LOG")
+        {
+            Jianli_1<LOG>(info[0], info[2]);
+        }
+        else if(info[1]=="TANH")
+        {
+            Jianli_1<TANH>(info[0], info[2]);
+        }
+        else if(info[1]=="EXP")
+        {
+            Jianli_1<EXP>(info[0], info[2]);
+        }
+        else if(info[1]=="SINGMOID")
+        {
+            Jianli_1<SINGMOID>(info[0], info[2]);
+        }
+        else if(info[1]=="Print")
+        {
+            Jianli_1<Print>(info[0], info[2]);
+        }
+        else if(info[1]=="COND")
+        {
+            Jianli_3<COND>(info[0], info[2], info[3], info[4]);
+        }
     }
     
     void Set_Graph::processing_Stage3()
--- Set_Graph.h
+++ Set_Graph.h
@@ -7,7 +7,8 @@
 //
 
 /*
- *4.13补充极小量eps
+ *4.13 Cai. 补充极小量eps
+ *4.21 Cai. 补全了Set_Graph:: Operation类所有构造函数
  */
 
 
@@ -17,6 +18,7 @@
 #include <map>
 #include "Const.h"
 #include "Operation.h"
+#include "Operation2.h"
 namespace Computational_Graph{
     class Set_Graph{
         typedef Graph_Node<float> GNode;
@@ -33,13 +35,99 @@ namespace Computational_Graph{
         {
             info = std::move(a);
         }
-        void processing_Stage3();                        //对输入字符串的处理  未完成
+        void processing_Stage3();//对输入字符串的处理  未完成
+        /*
+         注意这里是修改
         void ADD(const string& obj, const string& p1, const string& p2);                //加法操作
         void Multi(const string& obj, const string& p1, const string& p2);               //乘法操作
         void MINUS(const string& obj, const string& p1, const string& p2);               //减法操作
         void Div(const string& obj, const string& p1, const string& p2);                 //除法操作
-        void Print(const string& name);           //执行打印操作  未完成
+        void Print(const string& name);//执行打印操作  未完成
         //假装这里有一吨操作符
+        //add by Cai on 4.21
+        void Cond(const string& obj, const string &p1, const string& p2, const string& p3);//Cond 操作
+        void XIAOYU (const string& obj, const string& p1, const string& p2);
+        void XIAODENG (const string& obj, const string& p1, const string& p2);
+        void DAYU (const string& obj, const string& p1, const string& p2);
+        void DADENG (const string& obj, const string& p1, const string& p2);
+        void DENGYU (const string& obj, const string& p1, const string& p2);
+        void BUDENG (const string& obj, const string& p1, const string& p2);
+        
+        void Sin(const string& obj, const string &p1);
+        void Exp(const string& obj, const string &p1);
+        void Tanh(const string& obj, const string &p1);
+        void Log(const string& obj, const string &p1);
+        void Sigmoid(const string& obj, const string &p1);
+        void Print(const string& obj, const string &p1);
+         */
+        //----------------------------------------------------------------------------
+        //4.21 针对代码重复过于严重，我要换一个写法
+        template <class A>
+        void Jianli_2(const string& obj, const string& p1, const string& p2)
+        {
+            auto x = graph.find(obj);  auto pt1 = graph.find(p1); auto pt2 = graph.find(p2);
+            if(x!=graph.end())           //被重定义错误
+            {
+                std::cerr<<"False Redefination\n";
+            }
+            else if (pt2==graph.end()||pt1 == graph.end())
+            {
+                std::cerr<<"False Parameter defination\n";
+            }
+            else
+            {
+                graph[obj].Mode = Operator;
+                graph[obj].Nodename = obj;
+                BaseNode<float>* para1 = graph[p1].node.get();
+                BaseNode<float>* para2 = graph[p2].node.get();
+                graph[obj].node = std::make_shared<A>(obj,para1, para2);
+            }
+        }
+        template <class A>
+        void Jianli_1(const string& obj, const string& p1)
+        {
+            auto x = graph.find(obj);  auto pt1 = graph.find(p1);
+            if(x!=graph.end())           //被重定义错误
+            {
+                std::cerr<<"False Redefination\n";
+            }
+            else if (pt1 == graph.end())
+            {
+                std::cerr<<"False Parameter defination\n";
+            }
+            else
+            {
+                graph[obj].Mode = Operator;
+                graph[obj].Nodename = obj;
+                BaseNode<float>* para1 = graph[p1].node.get();
+                graph[obj].node = std::make_shared<A>(obj,para1);
+            }
+        }
+        template <class A>
+        void Jianli_3(const string& obj, const string& p1, const string& p2,const string & p3)
+        {
+            auto x = graph.find(obj);  auto pt1 = graph.find(p1); auto pt2 = graph.find(p2); auto pt3= graph.find(p3);
+            if(x!=graph.end())           //被重定义错误
+            {
+                std::cerr<<"False Redefination\n";
+            }
+            else if (pt3==graph.end()||pt2==graph.end()||pt1 == graph.end())
+            {
+                std::cerr<<"False Parameter defination\n";
+            }
+            else
+            {
+                graph[obj].Mode = Operator;
+                graph[obj].Nodename = obj;
+                BaseNode<float>* para1 = graph[p1].node.get();
+                BaseNode<float>* para2 = graph[p2].node.get();
+                BaseNode<float>* para3 = graph[p2].node.get();
+                graph[obj].node = std::make_shared<A>(obj,para1, para2,para3);
+            }
+        }
+        
+        
+        //----------------------------------------------------------------------------
         void Compute(const string& a)             //对于表达式进行计算
         {
              float b = graph[a].node->Forward();
