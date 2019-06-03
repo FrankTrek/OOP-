f1b675921e04952adaed501240b5a563e5566714
Lionjump0723 <49281380+Lionjump0723@users.noreply.github.com>
2019-06-03 16:02:55 +0800

    Update ComputationalGraph.cpp

--- OOP第二阶段/分组/鍒嗙粍/A/ComputationalGraph.cpp
+++ OOP第二阶段/分组/鍒嗙粍/A/ComputationalGraph.cpp
@@ -77,6 +77,193 @@ void ComputationalGraph::EmptyCall()
     TimeTag++;
 }
 
+//Add by Cai on 5.22
+
+
+
+void ComputationalGraph::workstage1() {
+    std::string ini;
+    std::getline(std::cin,ini);
+    int times= std::stoi(ini);
+    
+    while (times--) {
+        incision();
+        if(info[1]=="P") {
+            AddNode(new Placeholder(info[0]));
+        }
+        else if (info[1]=="C") {
+            AddNode(new Constant(info[0],std::stof(info[2])));
+        }
+        else if (info[1]=="V") {
+            AddNode(new Variable(info[0],std::stof(info[2])));
+        }
+        else {
+            std::cout << "ERROR: Unaccepted Input" << std::endl;
+        }
+    }
+}
+
+void ComputationalGraph::workstage2() {
+    std::string ini;
+    getline(std::cin, ini);
+    int times= std::stoi(ini);
+    
+    while (times--) {
+        incision();
+        if(info[2]=="+")                 //为加法;
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new PlusOperator(info[0],vs));
+        }
+        else if(info[2]=="-")                 //为乘法
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new MinusOperator(info[0],vs));
+        }
+        else if(info[2]=="*")            //为减法
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new MultipleOperator(info[0],vs));
+        }
+        else if(info[2]=="/")
+        {
+            //Div(info[0], info[1], info[3]);
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new DivisionOperator(info[0],vs));
+        }
+        else if(info[2]=="<")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new LessOperator(info[0],vs));
+        }
+        else if(info[2]=="<=")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new LessEqualOperator(info[0],vs));
+        }
+        else if(info[2]==">")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new GreaterOperator(info[0],vs));
+        }
+        else if(info[2]==">=")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new GreaterEqualOperator(info[0],vs));
+        }
+        else if(info[2]=="==")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new GreaterEqualOperator(info[0],vs));
+        }
+        else if(info[2]=="==")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[1]));
+            vs.push_back(Find(info[3]));
+            AddNode(new EqualOperator(info[0],vs));
+        }
+        else if(info[1]=="SIN")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[2]));
+            AddNode(new SinOperator(info[0],vs));
+        }
+        else if(info[1]=="LOG")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[2]));
+            AddNode(new LogOperator(info[0],vs));
+        }
+        else if(info[1]=="TANH")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[2]));
+            AddNode(new TanhOperator(info[0],vs));
+        }
+        else if(info[1]=="EXP")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[2]));
+            AddNode(new ExpOperator(info[0],vs));
+        }
+        else if(info[1]=="SIGMOID")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[2]));
+            AddNode(new SigmoidOperator(info[0],vs));
+        }
+        else if(info[1]=="PRINT" || info[1] == "Print")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[2]));
+            AddNode(new PrintOperator(info[0],vs,std::cout));
+        }
+        else if(info[1]=="COND")
+        {
+            std::vector<Node*> vs;
+            vs.push_back(Find(info[2]));
+            vs.push_back(Find(info[3]));
+            vs.push_back(Find(info[4]));
+            AddNode(new CondOperator(info[0],vs));
+        }
+    }
+}
+
+void ComputationalGraph::workstage3() {
+    std::string ini;
+    getline(std::cin, ini);
+    int times= std::stoi(ini);
+    
+    while (times--) {
+        incision();
+        if(info[0]=="EVAL") {
+            int m=info.size()-3;
+            std::vector< std::pair<std::string, float> >vc;
+            for(int i=0;i<m;i+=2) {
+                vc.push_back(std::make_pair(info[3+i], std::stof(info[4+i])));
+            }
+            Calc(info[1], vc, std::cout);
+            
+        }
+        else if (info[0]=="SETANSWER") {
+            EmptyCall();
+            int timetag=GetTime();
+            Find(info[1])->SetValue(PreAnswer[std::stoi(info[2])-1], timetag);
+            
+        }
+        else if (info[0]=="SETCONSTANT") {
+            EmptyCall();
+            int timetag=GetTime();
+            Find(info[1])->SetValue(std::stof(info[2]), timetag);
+        }
+        
+    }
+}
+
+
+//End Add
+
+
+
 //    析构计算图
 ComputationalGraph::~ComputationalGraph()
 {
