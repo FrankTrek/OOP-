a7e050329fa084dd81c724fb25d293120c817aed
SAZDDDDDD <48305798+Xiaoyu-SZ@users.noreply.github.com>
2019-06-07 10:49:53 +0800

    Update Newton.h

--- 2019.6.7/Newton.h
+++ 2019.6.7/Newton.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "Operator.h"
 #include "Scalar.h" 
+#include "Node.h"
 #include <vector>
 #include <iostream>
 //todo
-//设置ErrorSignal兼容报错 
+//璁剧疆ErrorSignal鍏煎鎶ラ敊 
 class Newton{
 	int n ;
+	int Timetag ;
+	float eps = 1.0e-7;//杩欐牱鍙紵
+	std::string ErrorSignal;
 	std::vector<Node*> Nodes1 ;
 	std::vector<Node*> Parameter ;
 	std::vector<Node*> Nodes2 ;
@@ -14,15 +18,16 @@ class Newton{
 	Node* res ;
 	Node* x ;
 public :
-	//搭建1-n次方节点
+	//鎼缓1-n娆℃柟鑺傜偣
 	Newton(int nn) ;
 	void Construct_Stage1() ;
 	void Construct_Stage2() ;
 	void Construct_Stage3() ;
 	void Construct_Stage4() ;
-	
-	float Cal() ;
-	
+	void Method();
+
+	float debug_Cal() ;
+	~Newton();
 //	void NewtonMethod() ;
 	
 };
