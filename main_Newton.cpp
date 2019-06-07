4304af7c5698d7764aa2a6f6adae2536e8a5005f
SAZDDDDDD <48305798+Xiaoyu-SZ@users.noreply.github.com>
2019-06-07 10:43:23 +0800

    Update main_Newton.cpp

--- main_Newton.cpp
+++ main_Newton.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
+#include "Operator.h"
+#include "Scalar.h"
 #include "Newton.h"
-int main(){
-	int n ;
-	std::cin >> n ;
-	Newton a(n) ;
-	std::cout<<a.Cal() ;
+
+int main() {
+
+	int nn;
+	std::cin >> nn;
+	Newton a(nn);
+	a.Construct_Stage1();
+	a.Construct_Stage2();
+	a.Construct_Stage3();
+	a.Construct_Stage4();
+	a.Method();
 }
