013626c8216cfd2e9111ca1c5067947da6226f54
Lionjump0723 <49281380+Lionjump0723@users.noreply.github.com>
2019-06-13 18:42:05 +0800

    Update Scalar.cpp

--- 2019.6.7/Scalar.cpp
+++ 2019.6.7/Scalar.cpp
@@ -4,17 +4,17 @@
 template <>
 void Placeholder<float>::Backward(float gradient, std::string & ErrorSignal)
 {
-    SetGradi(GetGradi()+gradient);
+    //SetGradi(GetGradi()+gradient);
     
 }
 template <>
 void Constant<float>::Backward(float gradient, std::string & ErrorSignal)
 {
-    SetGradi(GetGradi()+gradient);
+    //SetGradi(GetGradi()+gradient);
 }
 template <>
 void Variable<float>::Backward(float gradient, std::string & ErrorSignal)
 {
-    SetGradi(GetGradi()+gradient);
+    //SetGradi(GetGradi()+gradient);
     
 }
